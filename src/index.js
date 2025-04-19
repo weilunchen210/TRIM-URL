25a720e4ea042ca8f81dfb31fd9768110fb575e9
Chen Wei Lun <weilunchen210@gmail.com>
2025-04-19 19:33:22 +0800

    set up .env and mongodb connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import url  from './routes/urlRoutes.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 
 
 app.use('/api', url);
 
+console.log(process.env.MONGODB_URL)
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
